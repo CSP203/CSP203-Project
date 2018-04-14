fb122acd705669b73cff158e8cbbd2070e64bd56
adityamatt <33998232+adityamatt@users.noreply.github.com>
2018-04-14 19:21:23 +0530

    Signup works,Login works with sessions
    Sign up does not use sessions right now, means a logged in user can register new as well, will add that later
    Login uses sessions now and redirects to index.php on successful login, which when opened without logging in redirects back to login_page.php

---
+++ signup.php
@@ -0,0 +1,64 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $conn = mysqli_connect($servername, $username, $password,"oneclick");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    //echo "Connected successfully";
+    //echo "<br/>";
+   
+    //echo "<br/>";
+    $signup_first_name=$_POST['first_name'];
+    $signup_second_name=$_POST['second_name'];
+    $signup_email=$_POST['email'];
+    $signup_pass1=$_POST["pass1"];
+    $signup_pass2=$_POST["pass2"];
+    if ($signup_pass1!=$signup_pass2)
+    {
+        printf("Passwords do not match\nRedirecting back to signup page");
+            header("refresh:5; signup.html");
+        exit();
+    }
+    //printf("%s %s %s %s %s\n",$signup_first_name,$signup_second_name,$signup_email,$signup_pass1,$signup_pass2);
+    $isValid="Select all email from instructor";
+    //echo "<br>";    
+    //echo $isValid;
+    //echo "<br>";
+    $result = mysqli_query($conn,$isValid);
+    $count = mysqli_num_rows($result);
+    if (mysqli_num_rows($result)>0)
+    {
+        //echo $count;
+        //echo "<br/>";
+        while ($row=mysqli_fetch_row($result))
+        {
+            //printf("Comparing given email %s with %s \n",$signup_email,$row[0]);
+            //echo "<br/>";
+            if (strcmp($row[0],$signup_email)==0)
+            {
+                printf("The Email ID provided is already registered to another user\n");
+                //header("refresh: 5; signup.html");
+                exit();
+            }
+        }
+    }
+    //data given is valid
+    $enter_data="Insert into instructor(`IDPass`,`FirstName`,`LastName`,`email`) values('$signup_pass1','$signup_first_name','$signup_second_name','$signup_email')";
+    $result=$conn->query($enter_data);
+    //echo "<br/>";
+        
+    if ($result==true)
+    {
+        printf("Signed Up Sucessfully\n Redirecting to login page");
+        header("refresh: 5; login_page.php");
+    }
+    else 
+    {
+        printf("Error occured,Please Retry\nRedirecting to signup page");
+        echo "Error: " . $enter_data . "<br>" . $conn->error;
+        //header("refresh: 5; signup.html");
+    }
+    mysqli_close($conn);
+?>
