d75f8dd7e3168ffae52c2f38972e4097b6244682
Prerna Garg <33514735+prerna-grg@users.noreply.github.com>
2018-04-18 23:19:02 +0530

    header updated

--- student_portal/login_page.php
+++ student_portal/login_page.php
@@ -67,13 +67,31 @@ if(isset($_SESSION['login_user'])){
 
 	<div id="head-top" style="position: absolute; width: 100%; top: 0; ">
 		
+        <div class="gtco-top">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-6 col-xs-6">
+						<div id="gtco-logo"><a href="index.php">OneClick <em>.</em></a></div>
+					</div>
+					<div class="col-md-6 col-xs-6 social-icons">
+						<ul class="gtco-social-top">
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>	
+		</div>
+        
 		<nav class="gtco-nav sticky-banner" role="navigation">
 			<div class="gtco-container">
 				
 				<div class="row">
 					<div class="col-xs-12 text-center menu-1">
 						<ul>
-							<li><a href="login.html">Login</a></li>
+							<li  class="active"><a href="login.html">Login</a></li>
                             <li><a href="signup.html">SignUp</a></li>
                             <li><a href="index.html">Home</a></li>
 						</ul>
